9688f33e4103ff6fd0b4a5ff2efa87312c0fa30c
Jordan Sheppard <jg.sheppard808@gmail.com>
2026-04-29 13:44:23 -0700

    Encapuslated state vector and matrix arithmetic

--- src/solver.hpp
+++ src/solver.hpp
@@ -15,6 +15,28 @@ struct Mat4x4 {
         return data[r * 4 + c];
     }
 
+    // --- Matrix Addition ---
+    __host__ __device__
+    Mat4x4 operator+(const Mat4x4& other) const {
+        Mat4x4 result;
+        #pragma unroll
+        for(int i = 0; i < 16; i++) {
+            result.data[i] = this->data[i] + other.data[i];
+        }
+        return result;
+    }
+
+    // --- Matrix Subtraction ---
+    __host__ __device__
+    Mat4x4 operator-(const Mat4x4& other) const {
+        Mat4x4 result;
+        #pragma unroll
+        for(int i = 0; i < 16; i++) {
+            result.data[i] = this->data[i] + other.data[i];
+        }
+        return result;
+    }
+
     // --- Matrix Multiplication ---
     __host__ __device__
     Mat4x4 operator*(const Mat4x4& other) const {
@@ -33,15 +55,30 @@ struct Mat4x4 {
         }
         return result;
     }
+
+    // --- Scalar Multiplication --- 
+    __host__ __device__ Mat4x4 operator*(float scalar) const {
+        Mat4x4 result;
+        #pragma unroll
+        for(int i = 0; i < 16; i++) {
+            result.data[i] = this->data[i] * scalar;
+        }
+        return result;
+    }
 };
+
+// Makes scalar multiplication for Mat4x4 okay on both sides
+__host__ __device__
+inline Mat4x4 operator*(double scalar, const Mat4x4& M) {
+    return M * scalar;
+}
 
 
 struct VarState {
     float s[5];     // 4D physical state [theta, phi, l1, l2] + cost
-    float M[16];    // 4x4 sensitivity matrix to initial conditions
+    Mat4x4 M;       // 4x4 sensitivity matrix to initial conditions
 
     // --- Inline Reference Accessors --
-
     __host__ __device__ float& theta() { return s[0]; }
     __host__ __device__ const float& theta() const { return s[0]; }
 
@@ -60,22 +97,43 @@ struct VarState {
     // Overload Addition/Subtraction/Scalar Multiplication
     __host__ __device__ VarState operator+(const VarState& other) const {
         VarState result;
-        for(int i = 0; i < 5; i++) result.s[i] = this->s[i] + other.s[i];
-        for(int i = 0; i < 16; i++) result.M[i] = this->M[i] + other.M[i];
+
+        // Add states
+        #pragma unroll
+        for(int i = 0; i < 5; i++) {
+            result.s[i] = this->s[i] + other.s[i];
+        }
+
+        // Add sensitivity matrices
+        result.M = this->M + other.m;
         return result;
     }
 
     __host__ __device__ VarState operator-(const VarState& other) const {
         VarState result;
-        for(int i = 0; i < 5; i++) result.s[i] = this->s[i] - other.s[i];
-        for(int i = 0; i < 16; i++) result.M[i] = this->M[i] - other.M[i];
+
+        // Add states
+        #pragma unroll
+        for(int i = 0; i < 5; i++) {
+            result.s[i] = this->s[i] - other.s[i];
+        }
+
+        // Add sensitivity matrices
+        result.M = this->M - other.m;
         return result;
     }
 
     __host__ __device__ VarState operator*(float scalar) const {
         VarState result;
-        for(int i = 0; i < 5; i++) result.s[i] = this->s[i] * scalar;
-        for(int i = 0; i < 16; i++) result.M[i] = this->M[i] * scalar;
+
+        // Scalar multiply states
+        #pragma unroll
+        for(int i = 0; i < 5; i++) {
+            result.s[i] = this->s[i] * scalar;
+        }
+
+        // Scalar multiply matrices
+        result.M = this->M * scalar;
         return result;
     }
 };
