c0050c6abf730b3bc5391d0b10fb5ec9412aa126
Jordan Sheppard <jg.sheppard808@gmail.com>
2026-04-29 15:16:00 -0700

    Implemented compute_sensitivity_jacobian()

--- src/solver.cu
+++ src/solver.cu
@@ -16,29 +16,78 @@ __host__ __device__ void compute_state_physics(
     VarState& ds
 ) {
     // 1. Pull values locally using the struct accessors ()
-    double theta_val = state.theta();
-    double phi_val = state.phi();
-    double l1_val = state.l1();
-    double l2_val = state.l2();
+    double theta = state.theta();
+    double phi = state.phi();
+    double l1 = state.l1();
+    double l2 = state.l2();
+    double alpha = params.alpha;
 
     // 2. Trust the compiler optimizer to fuse these automatically!
-    double sin_t = sin(theta_val);
-    double cos_t = cos(theta_val);
+    double sin_t = sin(theta);
+    double cos_t = cos(theta);
 
     // Precompute squared terms
-    double cos2_t = cos_t * cos_t;
-    double l2_sq = l2_val * l2_val;
-    double phi_sq = phi_val * phi_val;
+    double cos_t_sq = cos_t * cos_t;
+    double l2_sq = l2 * l2;
+    double phi_sq = phi * phi;
 
     // 3. Evaluate RHS of the effective controlled pendulum dynamics
-    ds.theta() = phi_val;
-    ds.phi()   = sin_t - params.alpha * phi_val - l2_val * cos2_t;
-    ds.l1()    = -l2_sq * cos_t * sin_t - l2_val * cos_t - sin_t;
-    ds.l2()    = -phi_val - l1_val + params.alpha * l2_val;
+    ds.theta() = phi;
+    ds.phi()   = sin_t - alpha * phi - l2 * cos_t_sq;
+    ds.l1()    = -l2_sq * cos_t * sin_t - l2 * cos_t - sin_t;
+    ds.l2()    = -phi - l1 + alpha * l2;
     
     // Running Cost
-    ds.cost()  = 1.0 - cos_t + 0.5 * phi_sq + 0.5 * l2_sq * cos2_t;
+    ds.cost()  = 1.0 - cos_t + 0.5 * phi_sq + 0.5 * l2_sq * cos_t_sq;
 }
 
 
+__host__ __device__ Mat4x4 compute_sensitivity_jacobian(
+    const VarState& state,
+    const SystemParams& params
+) {
+    // 1. Pull values locally using the struct accessors ()
+    double theta = state.theta();
+    double phi = state.phi();
+    double l1 = state.l1();
+    double l2 = state.l2();
+    double alpha = params.alpha;
 
+    // 2. Trust the compiler optimizer to fuse these automatically!
+    double sin_t = sin(theta);
+    double cos_t = cos(theta);
+
+    // Precompute squared terms
+    double cos_t_sq = cos_t * cos_t;
+    double sin_t_sq  = sin_t * sin_t;
+    double l2_sq = l2 * l2;
+
+    // 3. Create sensitivity Jacobian matrix A(s)
+    Mat4x4 A;
+
+    // Row 0
+    A(0, 0) = 0.;
+    A(0, 1) = 1.;
+    A(0, 2) = 0.;
+    A(0, 3) = 0.;
+
+    // Row 1 
+    A(1, 0) = cos_t + 2 * l2 * cos_t * sin_t;
+    A(1, 1) = -alpha;
+    A(1, 2) = 0.;
+    A(1, 3) = -cos_t_sq;
+
+    // Row 2
+    A(2, 0) = -(l2_sq * (cos_t_sq - sin_t_sq) - l2 * sin_t + cos_t);
+    A(2, 1) = 0.;
+    A(2, 2) = 0.;
+    A(2, 3) = -(2 * l2 * cos_t * sin_t + cos_t);
+
+    // Row 3
+    A(3, 0) = 0.;
+    A(3, 1) = -1.;
+    A(3, 2) = -1.;
+    A(3, 3) = alpha;
+
+    return A;
+}
